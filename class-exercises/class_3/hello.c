63640d92f6bb3246aadf245d26baf72dedadd2e9
Vlad04 <vladiir.rod96@gmail.com>
2019-01-22 08:14:13 -0600

    Clase 3

---
+++ class-exercises/class_3/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    
+    int apple = 0;
+    
+    apple = 2+1;
+    
+    return 0;
+}
---
+++ class-exercises/class_3/macro_example.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define GRAVITY 9.81;
+//int global = 10;
+//The macro variable search on all the code and replace GRAVITY with the valio defined.
+int main(){
+    float result = 1000/GRAVITY;
+    printf("\nThis is an example of a macro\n");
+    printf("%f \n", result);
+    }
