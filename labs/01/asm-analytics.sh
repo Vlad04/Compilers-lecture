940aa1bd8b0e96bd98c44ca1acc9d51128a1a2a0
Vlad04 <vladiir.rod96@gmail.com>
2019-01-29 11:31:21 -0600

    A01129975 homework-01
    Signed-off-by: Vlad04 <vladiir.rod96@gmail.com>

--- labs/01/asm-analytics.sh
+++ labs/01/asm-analytics.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-make
-objdump -d simple_foo | less > myfile.txt
-echo "Hi, this is the output of the analysis:"
-echo "The number of instructions of './simple_foo' are " $(objdump -dw ./simple_foo | wc -l)
-echo "        You have 7 kind of instructions in this object file:"
-#objdump -d simple_foo | grep ' [0-9a-f]\{6\}:' | cut -b 33-50 | cut -d' ' -f1 | sort | uniq -c
 
-
+functions=$(grep -P -o '(?<=<)[a-zA-Z]*?(?=>:)' "$1")
+functions_count=$(echo "$functions" | wc -l)
+foo_instructions=$(awk -F"\n" -v RS="\n\n" '$1 ~ /foo/' "$1" | cut -b 32-50 | cut -d' ' -f1 | sed '/^$/d' | sed 's/[^a-zA-Z0-9]//g')
+main_instructions=$(awk -F"\n" -v RS="\n\n" '$1 ~ /main/' "$1" | cut -b 32-50 | cut -d' ' -f1 | sed '/^$/d'| sed 's/[^a-zA-Z0-9]//g')
+count_instructions=$(echo "$foo_instructions"$'\n'"$main_instructions"| sort -u | wc -l)
+echo "You have $count_instructions kinds of instructions on this object file: "
+echo "$foo_instructions"$'\n'"$main_instructions"|sort | uniq -c | awk '{print "\t"$2"\t: executed "$1" times"}'
+echo "You have $functions_count functions:"
+paste <(echo "$functions") <(grep -wF "$functions" "$1" | awk '{if ($1 !~ ":" && $1 != "Disassembly") {print $1}}') | awk '{print "\t"$1"\t: Located at "$2" addr"}'
+exit 0 
